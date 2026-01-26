9ad06e0854b61621a2484dbb0c4855090f14a60e
AlexLeeVT <95508161+AlexLeeVT@users.noreply.github.com>
2026-01-26 01:31:18 -0500

    updated flexibility

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
+from enum import StrEnum
+
 import httpx
 from urllib.parse import urlencode
 import json
-from pathlib import Path
 
-from enum import StrEnum, IntEnum
+from pathlib import Path
 
 class CourseType(StrEnum):
     undergraduate='coursetype_undergraduate',
     graduate='coursetype_graduate',
 
 class SubjectType(StrEnum):
-    ECE='ECE',
-    CS='CS',
+    ECE="ECE",
+    CS="CS",
 
 class CourseAvailability(StrEnum):
     OPEN_ONLY="A",
-    OPEN_OR_FULL="A%2CF%2CW",
+    OPEN_OR_FULL="A,F,W",
+
+class RouteType(StrEnum):
+    SEARCH="search",
+    DETAILS="details",
 
 url = "https://classes.vt.edu/api/"
-params = {
+search_params = {
     "page": "fose",
     "route": "search",
     "stat": CourseAvailability.OPEN_OR_FULL,
     CourseType.graduate: "Y",
     "subject": SubjectType.ECE,
 }
-url_params = urlencode(params)
+url_params = urlencode(search_params)
 
 payload = {
     "other": {
         "srcdb": "202601"
     },
     "criteria": [
-        {"field": "stat", "value": "A"},
-        {"field": "coursetype_graduate", "value": "Y"},
+        {"field": "stat", "value": CourseAvailability.OPEN_OR_FULL},
+        {"field": CourseType.graduate, "value": "Y"},
         {"field": "subject", "value": "ECE"},
     ],
 }
@@ -50,5 +55,4 @@ outputfile = Path("output.txt")
 with outputfile.open("w") as f:
     f.write(json)
 
-for classes in data['results']:
-    print(classes['code'])
+courses = data["results"]
