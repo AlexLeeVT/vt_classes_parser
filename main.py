d6d6d3a7c4906b41ba7c3ae3e282e7240eebeab9
AlexLeeVT <alexlee1017@vt.edu>
2026-01-27 13:18:00 -0500

    fixed enums

--- main.py
+++ main.py
@@ -6,53 +6,60 @@ import json
 
 from pathlib import Path
 
-class CourseType(StrEnum):
-    undergraduate='coursetype_undergraduate',
-    graduate='coursetype_graduate',
+class ProgramLevel(StrEnum):
+    UNDERGRAD='coursetype_undergraduate'
+    GRAD='coursetype_graduate'
 
-class SubjectType(StrEnum):
-    ECE="ECE",
-    CS="CS",
+class Discipline(StrEnum):
+    ECE="ECE"
+    CS="CS"
 
-class CourseAvailability(StrEnum):
-    OPEN_ONLY="A",
-    OPEN_OR_FULL="A,F,W",
+class Availability(StrEnum):
+    OPEN="A"
+    OPEN_OR_FULL="A,F,W"
 
-class RouteType(StrEnum):
-    SEARCH="search",
-    DETAILS="details",
+class Query(StrEnum):
+    SEARCH="search"
+    DETAILS="details"
 
-url = "https://classes.vt.edu/api/"
-search_params = {
-    "page": "fose",
-    "route": "search",
-    "stat": CourseAvailability.OPEN_OR_FULL,
-    CourseType.graduate: "Y",
-    "subject": SubjectType.ECE,
-}
-url_params = urlencode(search_params)
+def fetch_courses(program_level: ProgramLevel, discipline: Discipline, availability: Availability):
+    search_params = {
+        "page": "fose",
+        "route": Query.SEARCH,
+        "stat": availability,
+        program_level: "Y",
+        "subject": discipline,
+    }
+    payload = {
+        "other": {
+            "srcdb": "202601"
+        },
+        "criteria": [
+            {"field": "stat", "value": availability},
+            {"field": program_level, "value": "Y"},
+            {"field": "subject", "value": discipline},
+        ],
+    }
+    url = "https://classes.vt.edu/api/"
+    url_params = urlencode(search_params)
 
-payload = {
-    "other": {
-        "srcdb": "202601"
-    },
-    "criteria": [
-        {"field": "stat", "value": CourseAvailability.OPEN_OR_FULL},
-        {"field": CourseType.graduate, "value": "Y"},
-        {"field": "subject", "value": "ECE"},
-    ],
-}
+    with httpx.Client(timeout=10.0) as client:
+        try:
+            response = client.post(f"{url}?{url_params}",
+                                   json=payload,)
+            _ = response.raise_for_status()
 
-with httpx.Client(timeout=10.0) as client:
-    response = client.post(f"{url}?{url_params}",
-                           json=payload,)
+            return json.dumps(response.json(), indent=2)
 
-    response.raise_for_status()
-    data = response.json()
-    json = json.dumps(data, indent=2)
+        except httpx.HTTPError as exc:
+            print(f"HTTP Exception for {exc.request.url} - {exc}")
+            return None
 
-outputfile = Path("output.txt")
-with outputfile.open("w") as f:
-    f.write(json)
 
-courses = data["results"]
+
+
+# outputfile = Path("output.txt")
+# with outputfile.open("w") as f:
+#     f.write(json)
+# 
+# courses = data["results"]
